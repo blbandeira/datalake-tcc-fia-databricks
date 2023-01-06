f437454865ed86538a464e1d9f756a7b747a62c5
blbandeira <blbandeira>
2023-01-06 19:28:48 +0000

    added dbfs path

--- Ingestion/ingest-api.py
+++ Ingestion/ingest-api.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
 AIRFLOW_PARAM_TEXT = dbutils.widgets.get("param1")
 
-with open('/mnt/datalake/raw/hello_world.txt', 'w') as f:
+with open('/dbfs/mnt/datalake/raw/hello_world.txt', 'w') as f:
     f.write(AIRFLOW_PARAM_TEXT)
